dd1c44f295444142100082d8c41ff481fd58b393
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-12-02 23:38:19 +0200

    v-for loop for todos show the items listing

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <center>
+      <h1>hoidettavia asioita</h1>
+      <Todos v-bind:todos="todos" />
+    </center>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Hoida näyttödokumentit",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Tarkista tehtäväpalautukset",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
